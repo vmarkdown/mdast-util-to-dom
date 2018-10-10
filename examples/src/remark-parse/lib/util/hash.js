6f27072e642f3d0d9b9e6cdc63bc43b0f924dd56
yucopowo <yangxiuliang@moseeker.com>
2018-10-10 13:07:53 +0800

    self parse

--- examples/src/remark-parse/lib/util/hash.js
+++ examples/src/remark-parse/lib/util/hash.js
@@ -1,9 +1,9 @@
 module.exports = function hash(str) {
-    if(!str) return 0;
+    if (!str) return 0;
 
     str = String(str);
     var hash = 5381, i = str.length;
-    while(i) {
+    while (i) {
         hash = (hash * 33) ^ str.charCodeAt(--i);
     }
     return hash >>> 0;
